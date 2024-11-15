16d168963abbae195d0b1653efa8b4513c8840c8
selfishAltruism <officialkyus@gmail.com>
2024-11-15 23:30:01 +0900

    fix: build error

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,10 +1,1 @@
 export { Loading } from "./screen/Loading";
-
-export {
-  BlueButton,
-  OrangeButton,
-  OrangeTwoButton,
-  RedButton,
-} from "./element/Button";
-
-export { Title, SubTitle } from "./element/Text";
