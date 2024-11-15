84d6ff6fa27c516c41ffdb191015a3eefdae3025
selfishAltruism <officialkyus@gmail.com>
2024-11-15 23:33:38 +0900

    chore: loading update

--- src/entities/screen/Loading.tsx
+++ src/entities/screen/Loading.tsx
@@ -1,11 +1,20 @@
 import { BeatLoader } from "react-spinners";
+import styled from "@emotion/styled";
 
 import { colors } from "@/configs";
 
 export const Loading = () => {
   return (
-    <>
+    <Wrapper>
       <BeatLoader color={colors.black} size={30} speedMultiplier={1} />
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
